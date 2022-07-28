6be74679735f8ad38d0fd5f081bd73f0c1fa08b6
rifki <rifkisanitkj@gmail.com>
2022-07-28 23:20:05 +0700

    nama

--- app/src/main/java/com/example/proyek1/NamaActivity.java
+++ app/src/main/java/com/example/proyek1/NamaActivity.java
@@ -3,6 +3,10 @@ package com.example.proyek1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NamaActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class NamaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nama);
 
         getSupportActionBar().setTitle(R.string.nama);
+
+        EditText etNama = findViewById(R.id.et_nama);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ambil nilai etnama
+                String nama = etNama.getText().toString();
+                //tampilkan dalam toast
+                Toast.makeText(NamaActivity.this, "Hey, "+nama, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
