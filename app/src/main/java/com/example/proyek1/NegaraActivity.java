00b35fed8f2678187c765c9c07018ddbe17364f8
rifki <rifkisanitkj@gmail.com>
2022-07-28 23:40:47 +0700

    negara

--- app/src/main/java/com/example/proyek1/NegaraActivity.java
+++ app/src/main/java/com/example/proyek1/NegaraActivity.java
@@ -3,6 +3,10 @@ package com.example.proyek1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class NegaraActivity extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class NegaraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_negara);
 
         getSupportActionBar().setTitle(R.string.negara);
+
+        String[] negara = getResources().getStringArray(R.array.daftar_negara);
+
+        ListView lvNegara = findViewById(R.id.lv_negara);
+        lvNegara.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(NegaraActivity.this, negara[i]+" clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
